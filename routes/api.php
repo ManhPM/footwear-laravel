19fc3640ca106952989ae64d3bb23f51e500f4b6
ManhPM <phammanhbeo2001@gmail.com>
2024-04-07 17:40:56 +0700

    authorize for all routes

--- routes/api.php
+++ routes/api.php
@@ -29,24 +29,41 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::prefix('v1')->group(function () {
-        Route::post('cart-add', [CartController::class, 'store'])->middleware('check.product.exist');
-        Route::delete('cart-remove', [CartController::class, 'destroy'])->middleware('check.product.exist');
-        Route::put('cart-update', [CartController::class, 'update'])->middleware('check.product.exist');
+        Route::post('cart-add', [CartController::class, 'store']);
+        Route::delete('cart-remove', [CartController::class, 'destroy']);
+        Route::put('cart-update', [CartController::class, 'update']);
         Route::get('cart', [CartController::class, 'index']);
-        Route::post('checkout', [CartController::class, 'processCheckout'])->middleware(['check.coupon.exist', 'check.pending.order']);
+        Route::post('checkout', [CartController::class, 'processCheckout'])->middleware(['check.pending.order']);
 
-        // Route::resource('roles', RoleController::class)->middleware('permission:show-coupon');
-        Route::get('roles', [RoleController::class, 'index'])->middleware('permission:show-coupon');
-        Route::resource('users', UserController::class);
-        Route::resource('categories', CategoryController::class);
-        Route::resource('products', ProductController::class);
-        Route::resource('coupons', CouponController::class);
+        Route::get('roles', [RoleController::class, 'index'])->middleware('permission:show-role');
+        Route::post('roles', [RoleController::class, 'store'])->middleware('permission:create-role');
+        Route::put('roles/{id}', [RoleController::class, 'update'])->middleware('permission:update-role', 'check.role.exist');
+        Route::delete('roles/{id}', [RoleController::class, 'destroy'])->middleware('permission:delete-role', 'check.role.exist');
 
-        Route::get('/orders/confirm/{id}', [OrderController::class, 'confirmOrder'])->middleware(['permission:employee']);
-        Route::get('/orders/cancel/{id}', [OrderController::class, 'cancelOrder'])->middleware(['permission:employee']);
-        Route::get('/orders', [OrderController::class, 'index'])->middleware(['permission:employee']);
-        Route::get('/orders/detail/{id}', [OrderController::class, 'getDetailOrder'])->middleware(['check.order.exist']);
-        Route::get('/orders/cancel/{id}', [OrderController::class, 'cancelOrder'])->middleware(['permission:employee']);
+        Route::get('users', [UserController::class, 'index'])->middleware('permission:show-user');
+        Route::post('users', [UserController::class, 'store'])->middleware('permission:create-user');
+        Route::put('users/{id}', [UserController::class, 'update'])->middleware('permission:update-user', 'check.user.exist');
+        Route::delete('users/{id}', [UserController::class, 'destroy'])->middleware('permission:delete-user', 'check.user.exist');
+
+        Route::get('categories', [CategoryController::class, 'index'])->middleware('permission:show-category');
+        Route::post('categories', [CategoryController::class, 'store'])->middleware('permission:create-category');
+        Route::put('categories/{id}', [CategoryController::class, 'update'])->middleware('permission:update-category', 'check.category.exist');
+        Route::delete('categories/{id}', [CategoryController::class, 'destroy'])->middleware('permission:delete-category', 'check.category.exist');
+
+        Route::get('products', [ProductController::class, 'index'])->middleware('permission:show-product');
+        Route::post('products', [ProductController::class, 'store'])->middleware('permission:create-product');
+        Route::put('products/{id}', [ProductController::class, 'update'])->middleware('permission:update-product', 'check.product.exist');
+        Route::delete('products/{id}', [ProductController::class, 'destroy'])->middleware('permission:delete-product', 'check.product.exist');
+
+        Route::get('coupons', [CouponController::class, 'index'])->middleware('permission:show-coupon');
+        Route::post('coupons', [CouponController::class, 'store'])->middleware('permission:create-coupon');
+        Route::put('coupons/{id}', [CouponController::class, 'update'])->middleware('permission:update-coupon', 'check.coupon.exist');
+        Route::delete('coupons/{id}', [CouponController::class, 'destroy'])->middleware('permission:delete-coupon', 'check.coupon.exist');
+
+        Route::get('/orders/confirm/{id}', [OrderController::class, 'confirmOrder'])->middleware('permission:employee', 'check.order.exist');
+        Route::get('/orders/cancel/{id}', [OrderController::class, 'cancelOrder'])->middleware('permission:employee', 'check.order.exist');
+        Route::get('/orders', [OrderController::class, 'index'])->middleware('permission:employee');
+        Route::get('/orders/detail/{id}', [OrderController::class, 'getDetailOrder'])->middleware('permission:employee', 'check.order.exist');
 
         Route::get('/logout', [AuthController::class, 'logout']);
         Route::get('/profile', [AuthController::class, 'profile']);
@@ -54,9 +71,8 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 });
 Route::prefix('v1')->group(function () {
-    Route::get('categories/{id}', [CategoryController::class, 'getProductsByCategoryId']);
-    Route::get('products/{id}', [ProductController::class, 'show']);
-    Route::get('products/{id}', [AuthController::class, 'show']);
+    Route::get('categories/{id}', [CategoryController::class, 'getProductsByCategoryId'])->middleware('check.category.exist');
+    Route::get('products/{id}', [ProductController::class, 'show'])->middleware('check.product.exist');
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
 
